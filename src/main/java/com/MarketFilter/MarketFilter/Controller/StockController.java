5260299199f40dcf09f795d33ce8b8dcbaceacf2
sanjay <sanjay.reddy@dreamtekis.com>
2024-08-17 17:54:09 +0530

    Added CORS support to StockController

--- src/main/java/com/MarketFilter/MarketFilter/Controller/StockController.java
+++ src/main/java/com/MarketFilter/MarketFilter/Controller/StockController.java
@@ -1,6 +1,7 @@
 package com.MarketFilter.MarketFilter.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.MarketFilter.MarketFilter.Service.StockService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/stocks")
 public class StockController {
 
